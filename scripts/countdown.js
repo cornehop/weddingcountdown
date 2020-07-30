57922496f6bec38b5e9f43e25a6c08abdd7e08ae
Corne Hop <itguy@honeyside.org>
2020-07-30 20:34:17 +0200

    init

---
+++ scripts/countdown.js
@@ -0,0 +1,36 @@
+var countdowndate = new Date('2020-07-17 14:00:00').getTime();
+
+let isPlaying = false;
+function playAudio() {
+  var audioSource = document.getElementById("myAudio");
+  if (isPlaying) {
+    audioSource.pause();
+  } else {
+    audioSource.play();
+  }
+  isPlaying = !isPlaying;
+};
+
+var x = setInterval(function() {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countdowndate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = ("0" + Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).slice(-2);
+  var minutes = ("0" + Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))).slice(-2);
+  var seconds = ("0" + Math.floor((distance % (1000 * 60)) / 1000)).slice(-2);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("counter").innerHTML = days + "d " + hours + "u "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("counter").innerHTML = "I DO!";
+  }
+}, 1000);
